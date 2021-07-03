2668783d1cf8aa428762f719e3d97058e4c9b3cd
Hamza Shaikh <53708485+rewazzu@users.noreply.github.com>
2021-07-02 22:04:39 -0700

    Updated signal velocity

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,26 +5,26 @@ using DG.Tweening;
 public class GameManager : MonoBehaviour {
 
 	public GameObject[] Units = new GameObject[4];
-    public GameObject NextUnit; // index used to keep track of the unit after the current unit
+  public GameObject NextUnit; // index used to keep track of the unit after the current unit
 	public GameObject ActiveUnit; // the unit currently allowed to move
-    public GameObject deadpulser; // The queen
-    public GameObject activation_signal; // once the signal reaches a unit, it should be able to move
-    public float activation_frequency = 60;
-    public float activation_period = 0;
+  public GameObject deadpulser; // The queen
+  public GameObject activation_signal; // once the signal reaches a unit, it should be able to move
+  public float activation_frequency = 30;
+  public float activation_period = 0;
 
 
-    private int _activeUnitIndex; // the index of the active unit
-    private int _nextUnitIndex = 1; // the index of the next unit set equal to one to always be 1 ahead of the active index
-    private GameObject signalClone;
-    
+  private int _activeUnitIndex; // the index of the active unit
+  private int _nextUnitIndex = 1; // the index of the next unit set equal to one to always be 1 ahead of the active index
+  private GameObject signalClone;
 
-    public AudioClip _beat;
-    public AudioSource _beatsource;
 
-    private float _currentInterval = 0; // the intitiation of the timing
-    private float _deadpulse = 0; // be one second off the unit control
+  public AudioClip _beat;
+  public AudioSource _beatsource;
 
-    public static GameManager Instance; 
+  private float _currentInterval = 0; // the intitiation of the timing
+  private float _deadpulse = 0; // be one second off the unit control
+
+  public static GameManager Instance;
 
     // Use this for initialization
     void Awake ()
@@ -34,18 +34,17 @@ public class GameManager : MonoBehaviour {
         ActiveUnit = Units [0]; //starts with the first unit in the array
         NextUnit = Units[1];
         Instance = this;
-
-	}
+		}
 
 	void Update () {
 
 		_currentInterval += Time.deltaTime; //records the time passed
         _deadpulse += Time.deltaTime; //records an offset time passed
-        
+
 
 		if (_currentInterval >= activation_period)
         {
-            
+
             _activeUnitIndex++;
             _nextUnitIndex++;
 			if (_activeUnitIndex == this.Units.Length) //Once you get through all the units restart
@@ -60,7 +59,7 @@ public class GameManager : MonoBehaviour {
             ActiveUnit = Units[_activeUnitIndex];
             NextUnit = Units[_nextUnitIndex];
 
-     
+
 			_currentInterval = 0;
 
             // pop the unit when selected
@@ -70,7 +69,7 @@ public class GameManager : MonoBehaviour {
             {
                 ActiveUnit.transform.localScale = new Vector3(1, 1, 1);
             });
-        
+
 		}
         if (_deadpulse >= activation_period/2) // it has to twice the activation frequency
         {
@@ -80,7 +79,7 @@ public class GameManager : MonoBehaviour {
             });
             _deadpulse = 0-(activation_period/2); // it has to be offset by .5 seconds
             signalClone = Instantiate(activation_signal, deadpulser.transform.position, Quaternion.identity); //creates the signal to the units at the location of the queen sprite
-           
+
         }
 
 
--- Assets/Script/activation_signal.cs
+++ Assets/Script/activation_signal.cs
@@ -6,12 +6,13 @@ public class activation_signal : MonoBehaviour {
 
     private float velx = 0;
     private float vely = 0;
+    private float time_delta = 0;
     private Vector3 activeunit_POS = new Vector3(0, 0, 0);
     //private float _magnitude;
-   
+
 
     Rigidbody2D rb;
- 
+
 
     public GameObject AU;
 
@@ -22,21 +23,22 @@ public class activation_signal : MonoBehaviour {
         rb.mass = 0; //so it won't inact a force on the units
         AU = GameManager.Instance.NextUnit; //instaniate active game object
         activeunit_POS = AU.transform.position; //get active game object position
+        time_delta = GameManager.Instance.activation_period/2; //two because its half a beat
 // old way
-        velx = activeunit_POS.x - transform.position.x;
-        vely = activeunit_POS.y - transform.position.y;
+        velx = (activeunit_POS.x - transform.position.x)/time_delta;
+        vely = (activeunit_POS.y - transform.position.y)/time_delta;
 
         // end old way
 
        // _magnitude = Mathf.Sqrt(Mathf.Pow((AU.transform.position.x - transform.position.x),2) + Mathf.Pow((AU.transform.position.y - transform.position.y),2));
-        
+
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
     {
-        rb.velocity = new Vector3 (velx*2, vely*2, 0); // takes 2 seconds to activate each unit
-        
-        
+        rb.velocity = new Vector3 (velx, vely, 0); // takes 2 seconds to activate each unit
+
+
     }
 }
